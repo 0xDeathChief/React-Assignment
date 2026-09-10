798bda5f223ddef66d4bc029202d08ca6fd3bfe1
Death Chief <azmainfayek2000@gmail.com>
2026-09-10 20:55:56 +0600

    Nav bar creation done

--- react-assignment/src/components/Nav.tsx
+++ react-assignment/src/components/Nav.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Logo from "../../../B14-A05-DevStack/assets/logo-text.png";
 const Nav = () => {
+    
+    const [active, setActive] = useState("home");
+
     return (
-        <div>
-            <h2 className='text-2xl'>Hello world</h2>
+        <div className='border-b border-[#F1F5F9] h-20'>
+            <nav className='container mx-auto h-full'>
+                <div className='flex justify-between items-center h-full font-[Plus Jakarta Sans]'>
+                    <img src={Logo} alt="Logo" />
+
+                    <ul className='flex justify-between gap-[29px]'>
+                        <li onClick={() => setActive("home")} className={active === 'home' ? "text-[#DB2777]": " "}>Home</li>
+                        <li onClick={() => setActive("technology")} className={active === 'technology' ? "text-[#DB2777]": " "}>Technology</li>
+                        <li onClick={() => setActive("projects")} className={active === 'projects' ? "text-[#DB2777]": " "}>Projects</li>
+                        <li onClick={() => setActive("about")} className={active === 'about' ? "text-[#DB2777]": " "}>About</li>
+                        <li onClick={() => setActive("contact")} className={active === 'contact' ? "text-[#DB2777]": " "}>Contact</li>
+                    </ul>
+
+                    <div className='flex gap-3'>
+                        <button className=''>Sign In</button>
+                        <button className="btn btn-secondary rounded-full w-23">Sign Up</button>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
