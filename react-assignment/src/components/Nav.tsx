2f9ccdf9d4256da4ba069b7dde3ad6af33cb267b
Death Chief <azmainfayek2000@gmail.com>
2026-09-10 20:28:58 +0600

    Initialized all package

---
+++ react-assignment/src/components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <h2 className='text-2xl'>Hello world</h2>
+        </div>
+    );
+};
+
+export default Nav;
